b45e3a312679465610a6b7135842b8cdb750c649
shimwabobs <shimwabonheur00@gmail.com>
2024-11-30 22:01:09 +0200

    Update

--- src/controllers/ordercontroller.js
+++ src/controllers/ordercontroller.js
@@ -1,24 +1,26 @@
 import Order from "../models/order.js";
+import Product from "../models/product.js";
 
 const OrderController={
     makeOrder:async(req,res)=>{
         try{
     
-            const{customer_name,customer_email,id,quantity}=req.body;
-            const product= await Product.findOne({where:{id:id},attributes:['stock','price']});
+            const{customer_name,customer_email,productId,quantity}=req.body;
+            const product= await Product.findOne({where:{id:productId},attributes:['stock','price']});
             if(!product){
-                res.status(404).json({message:"Product not found"});
+               return res.status(404).json({message:"Product not found"});
             }
             if(quantity>product.stock){
-                res.status(404).json({message:"Insufficient stock"});
+                return res.status(404).json({message:"Insufficient stock"});
             }
             const totalprice= parseFloat(product.price*quantity).toFixed(2);
-            const result=await Order.create({customer_name,customer_email,id,quantity,totalprice});
+            const result=await Order.create({customer_name,customer_email,productId,quantity,totalprice});
             const remainStock=product.stock-quantity
-            const updatedStock=await Product.update({stock:remainStock},{where:{id:id}});
-            res.status(201).json(result)
+            const updatedStock=await Product.update({stock:remainStock},{where:{id:productId}});
+            return res.status(201).json(result)
         }catch(error){
-            res.status(500).json({
+            console.log(error);
+           return res.status(500).json({
                 message:"Error to add",
                 error:error.message
             })
